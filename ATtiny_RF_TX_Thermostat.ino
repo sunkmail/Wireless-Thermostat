39518f4257043a602f232dc65fd92ad416d961e6
Scott <sunkmail@gmail.com>
2018-02-13 06:58:14 -0800

    Update ATtiny_RF_TX_Thermostat.ino

--- ATtiny_RF_TX_Thermostat.ino
+++ ATtiny_RF_TX_Thermostat.ino
@@ -1,3 +1,7 @@
+
+
+
+
 /*
 Small module to send a signal to activate a radiator solinoid when below adjustable threshold.  (A Thermostat)
 
@@ -13,44 +17,95 @@ The Radiator unit, a distance from this Thermostat unit, can then switch in a lo
   // https://andreasrohner.at/posts/Electronics/New-Arduino-library-for-433-Mhz-AM-Radio-Modules/
 
 // TinySnore - Version: Latest 
-#include <tinysnore.h>
+//#include <tinysnore.h>
   // Send ATtiny to sleep in one line
-  
-// TinyWireM - Version: Latest 
-#include <TinyWireM.h>
-#include <USI_TWI_Master.h>
 
+/*  
+// SSD1306_minimal - Version: Latest 
+#include <SSD1306_minimal.h>
+*/
+
+/*
+// Tiny4kOLED - Version: Latest 
+#include <Tiny4kOLED.h>
+#include <font6x8.h>
+#include <font8x16.h>
+
+// TinyWireM - Version: Latest          // Not Needed with u8g2 / U8x8 - To Confirm
+                                        // Is needed for Tiny4k
+#include <TinyWireM.h>            // I2C Master lib for ATTinys which use USI
+#include <USI_TWI_Master.h>
+*/
+
+/*
 // U8g2 - Version: Latest   // I2C Displays
-#include <U8g2lib.h>
+//#include <U8g2lib.h>  Try using just the 8x8 - less memory usage
 #include <U8x8lib.h>
+//        8x8 seems to use up about 4k!  - Just #include statement increases by 1k ??
+//  May need to try:
+//          adafruit SSD1306 library???
+//          https://github.com/lexus2k/ssd1306    https://bitbucket.org/scargill/ssd1306  *
+//          Tiny4k
+//          TinyOzOLED
+//        Maybe :  https://tinusaur.org/projects/ssd1306xled/
+//        Also Maybe: https://github.com/greiman/SSD1306Ascii  *
+//        or: http://www.didel.com/OledLib.pdf    (https://github.com/nicoud/Oled)
+*/
+
 
 // DallasTemperature - Version: Latest 
-#include <DallasTemperature.h>
+#include <DallasTemperature.h>            // Has Git comments on how to cut down code size
 #include <OneWire.h>
 
 
 //  PIN Reference  http://highlowtech.org/?p=1695
 
+const byte pot_PIN = A2;                   // Pin to Adjustment Potentiometer
+
+//const byte SCL_PIN = 2;                   // I2C Pins
+//const byte SDA_PIN = 0;
+
+
+// **************************************************************
+// ****************** Module Set-ups ****************************
+
+// 0.91" OLED display module  128x32
+
+/*
+const byte contrast = 255;
+  // Constructor //    // U8G2_SSD1306_128X32_UNIVISION_1_HW_I2C(rotation, [reset [, clock, data]])
+    // U8G2_SSD1306_128X32_UNIVISION_1_HW_I2C OLED(U8G2_R0);  // roation only setting due to HW I2C and no reset pin  // only if using U8G2lib
+
+U8X8_SSD1306_128X32_UNIVISION_HW_I2C OLED;  // Setup U8x8  - No Rotation setting - Flip MAY be avalable as a separate command
+//  Just constructor adds 2.4k
+*/
+
 
 // RF Rx Module (433 MHz ASK Module)
 const byte RadioOut_PIN = 1; 
+
 const byte RFNodeID = 1;                    // Set this unit to Node # expeected by Rx unit  - Rx unit uses this to confirm valid message
+
 byte *RFData = "heat bedroom";                           // Activate Heat message expected by Rx
+
 const byte MaxRFDataSize = 15;
+
 RFTransmitter RFTx(RadioOut_PIN,RFNodeID);               // Set up RF Tx library on RadioOut Pin, set NodeID
 
 
 
-// Temperature Sensor (DS18B20)   
+// Temperature Sensor (DS18B20)       ******************** May need to use only onewire and manually send data *************************
+//                                                DallasTemperature may be too big and not ATtiny Compatible
+//                                                http://sheepdogguides.com/arduino/ar3ne1tt.htm may be useful
+
+
 const byte TempSense_PIN = 3;             // Pin to Temperature Sensor
 
-/* Constructor */
+      /* Constructor */
 OneWire oneWire(TempSense_PIN);           // Setup a oneWire instance to communicate with any OneWire devices  
                                           // (not just Maxim/Dallas temperature ICs)
 DallasTemperature tempSensor(&oneWire);   // Pass our oneWire reference to Dallas Temperature Library. 
 
-//const byte TempSensorCount = 1;           // How many sensors are we expecting      // not needed if only using 1
-byte tempSensorAddr[8];                   // Variable to store device's uniquie ID
 const byte TempSensorResolution = 9;      // Integer value for sensor precision.  Can be 9, 10, 11 or 12 bits
 /*
           Mode      Resol   Conversion time
@@ -59,19 +114,14 @@ const byte TempSensorResolution = 9;      // Integer value for sensor precision.
           11 bits   0.125°C   375 ms
           12 bits   0.0625°C  750 ms
 */
-const unsigned int TempSensorConvTime = 100;  // Time in ms to wait between request for temp conv. and read of temperature - Based on info above.
+const unsigned int TempSensorConvTime = 100;  // Min. Time in ms to wait between request for temp conv. and read of temperature - Based on info above.
+
 float currentTempC = 0.0;                 // Current Room Temperature in C
-// byte targetTempC = 21;                    // Target Room Temperature (for thermostat control)
-byte tempHysteresis = 2;                  // To prevent frequent On/Off cycles near target temp
-//const byte Hysterisis = 3;                  // Difference in temp between stop heating and starting again
+const byte tempHysteresis = 2;                  // To prevent frequent On/Off cycles near target temp               
+                                          // Difference in temp between stop heating and starting again
 
 
 // Other Stuff
-
-const byte pot_PIN = A2;                   // Pin to Adjustment Potentiometer
-
-const byte SCL_PIN = 2;                   // I2C Pins
-const byte SDA_PIN = 0;
 
 unsigned long currentMillis;      // Working/scratchpad variable for checking times
 
@@ -84,27 +134,40 @@ void setup() {
   pinMode(pot_PIN, INPUT);
 
   tempSensor.begin();                                 // Start the Library
-  tempSensor.getAddress(tempSensorAddr, 0);           // Get addrress for device at index 0  (only sensor, no loop needed)
-                                                        // Addresses used as faster than doing by index.
+  tempSensor.setWaitForConversion(false);             // Put into Async. Mode
+  tempSensor.requestTemperatures();             // Send request for current temp - To get initial value going
+  
+  
+/*  
+  TinyWireM.begin();      // Start I2C Library
+  oled.begin();             // Start up tiny4kOLED
+  oled.clear();
+  oled.on();
+  oled.switchRenderFrame();
+*/
 
-    tempSensor.setResolution(tempSensorAddr, TempSensorResolution);     // Set resolution of temp sensor
-    tempSensor.setWaitForConversion(false);             // Put into Async. Mode
-    // tempSensor.setCheckForConversion(true);             // Program will look for flag that conversion complete    //NOT ACTUALLY SURE what this setting does.  Will just track millis
-    tempSensor.requestTemperaturesByAddress(tempSensorAddr);    // Send request for current temp - To get initial value going
+/*
+  OLED.begin();                                   // Start Display Library
+  OLED.setFont(u8x8_font_pressstart2p_u);         // Set Prefered Font
+  OLED.setContrast(contrast);                     // Set display Contrast
+*/
   delay(100);
 }
 
 
 
 void loop() {
-  int setpointTempInC = map(analogRead(pot_PIN), 0, 1023, 15, 35);          // Target Temp in Degrees C
+  // byte setpointTempC = map(analogRead(pot_PIN), 0, 1023, 15, 35);          // Target Temp in Degrees C, Set by Pot    - Left out for initial testing
+  byte setpointTempC = 21;      // Starting point for testing
 
   checkRoomTemp();                  // get current temp
 
+  if(currentTempC < (setpointTempC + tempHysteresis)){
+      RFTx.send((byte *)RFData, strlen(RFData) + 1);
+  }  
   
-    RFTx.send((byte *)RFData, strlen(RFData) + 1);
-    
     delay(20000);                                 // Resend message every 20s
+                            // Make non-blocking to allow for pot value adjustment/screen updates
 }
 
 
@@ -116,9 +179,9 @@ void checkRoomTemp(){
   static unsigned long lastTempCheck = 0;                       // keep track of when last converstion started
   
   if(currentMillis - lastTempCheck > TempSensorConvTime){         // if the last read was more than conversion time: 
-    currentTempC = tempSensor.getTempC(tempSensorAddr);           // Read Current Temperature in Degrees C 
-    tempSensor.requestTemperaturesByAddress(tempSensorAddr);    // Send request for new temp conversion
-    lastTempCheck = currentMillis;                                // reset timer reference for conversion
-//    updateDisplay_FLAG = true;                                       // Set flag to trigger display update
+   
+    currentTempC = tempSensor.getTempCByIndex(0);             // Read Current Temperature in Degrees C     
+    tempSensor.requestTemperatures();                         // Send request for new temp conversion
+    lastTempCheck = currentMillis;                            // reset timer reference for conversion
   }
 }
