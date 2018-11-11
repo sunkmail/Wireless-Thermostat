121cc9ff6173a9b4baab8b897670712c01a643bc
Scott <sunkmail@gmail.com>
2018-11-10 19:08:56 -0800

    Update B_Setup.ino

--- Remote Node/B_Setup.ino
+++ Remote Node/B_Setup.ino
@@ -8,13 +8,32 @@ void setup() {
 
 
   // ************* Temp Sensor Start & Setup ********************
-  sensors.begin();
-  sensors.getAddress(Thermometer_Address, 0);
-  sensors.setResolution(Thermometer_Address, Resolution);    // set the resolution
-  sensors.setWaitForConversion(false);
+//  sensors.begin();
+//  sensors.getAddress(Thermometer_Address, 0);
+//  sensors.setResolution(Thermometer_Address, Resolution);    // set the resolution
+//  sensors.setWaitForConversion(false);
+//
+//  sensors.requestTemperatures();
 
-  sensors.requestTemperatures();
-  //  delayInMillis = 750 / (1 << (12 - Resolution));
+    // Set resolution ... should be correct by default, but just in case. :)
+  oneWire.reset(); 
+  oneWire.write(0xCC);  // Bypass ROM command - since only 1 sensor used
+  oneWire.write(0x4E);  // Write to scratchpad command
+  oneWire.write(0);     // Write Dummy value to alarm1 register
+  oneWire.write(100);   // Write Dummy value to alarm2 register
+  oneWire.write(0x7F);  // set resolution to 12
+  delay(10);
+  oneWire.reset(); 
+  
+// Do a read eeprom/scratchpad test on bootup to check resolution.  if already correct, do bother with above.
+//  only impliment after above is working.
+
+
+  oneWire.reset(); 
+  oneWire.write(0xCC);  // Bypass ROM command - since only 1 sensor used
+  oneWire.write(0x44);  // Request Temp Conversion
+
+  //  delayInMillis = 750 / (1 << (12 - Resolution));   // Added as a const for 12 bit res.
   lastTempRequest = millis();
 
 
@@ -29,7 +48,7 @@ void setup() {
 
   OzOled.sendCommand(0xC8);   // Vertically Flip Display  (Default is 0xC0)
   OzOled.sendCommand(0xA1);   //                          (Default is 0xA0)
-  
+
 
   OzOled.setBrightness(Brightness);         // Set Brightness
   displayPreCharge(PreCharge1, PreCharge2); // To absolute min
@@ -41,7 +60,7 @@ void setup() {
 
 
   // ************** RF Rx Module (433 MHz ASK Module) ******************
-//    Nothing to Set-up
+  //    Nothing to Set-up
 
   // Ready to go!
 
