dd62668c700bba86e7303fda357191c536a46bc3
Scott <sunkmail@gmail.com>
2018-11-14 16:20:01 -0800

    Update D_Functions.ino

--- Remote Node/D_Functions.ino
+++ Remote Node/D_Functions.ino
@@ -9,32 +9,22 @@ void updateDisplay() {
   byte x = 4;             // X position for display start
   const byte y = 2;       // Y position for display
 
-  byte dataTemp = 0;    // for tracking how many characters have printed, to update X location
-
-
   if (tempPos == true)
     OzOled.printChar(' ', x, y);    // If number is positive, just leave a Blank
   else
     OzOled.printChar('-', x, y);    // If number is negative, place a minus sign
   x++;                              // Increment for next character(s)
 
-  OzOled.printNumber(long(mDegreeC / 1000), x, y);  // Print the whole degree portion
+  OzOled.printNumber(long(wholeDegreeC), x, y);  // Print the whole degree portion
   x++;                              // Increment for next character(s)
-  if (mDegreeC / 1000 >= 10)        // if 2 digit whole degrees temp, add extra space for next print
+  if (wholeDegreeC >= 10)           // if 2 digit whole degrees temp, add extra space for next print
     x++;                            // Increment for next character(s)
 
   OzOled.printChar('.', x, y);      // Print decimal character
   x++;                              // Increment for next character(s)
 
-
-  dataTemp = (mDegreeC % 1000);       // Parse out decimal portion
-  if (dataTemp % 100 >= 50)           // if the 2nd digit is 5 or higher,
-    dataTemp = (dataTemp / 100) + 1;  // round up the first decimal digit
-  else
-    dataTemp = (dataTemp / 100);      // otherwise just get first decimal
-
-  OzOled.printNumber(long(dataTemp), x, y);  // Print the whole degree portion
-  x++;                              // Increment for next character(s)
+  OzOled.printNumber(long(decimalDegreeC), x, y);  // Print the decimal degrees portion
+  x++;                                              // Increment for next character(s)
 
   OzOled.printChar(' ', x, y);      // Print 'space' character - overwrite any old data
   x++;                              // Increment for next character(s)
@@ -43,9 +33,10 @@ void updateDisplay() {
   x++;                              // Increment for next character(s)
 
   for (byte i = 1; i >= 3; i++) {   // Print multiple 'space' characters to overwrite any old data
-    OzOled.printChar(' ', x, y);    
-    x++;                            
+    OzOled.printChar(' ', x, y);
+    x++;
   }
+
 
   //char tmp[6];      // Array to hold Characters, assuming -XX.Y to be largest length + 1 for Null
   //char tmp[15] = {'-','1','2','.','3','4'};
@@ -54,12 +45,14 @@ void updateDisplay() {
 
   //  char tmp[8] = {'1','2','3','4','5','6'};
 
-  /*  Can't get to compile - should be right -> undefined reference ??
+  /*  // Can't get to compile - should be right -> undefined reference ??
 
   */
   //  OzOled.printBigNumber(tmp, 0, 0, 5);  // Print Large Font up to 5 Chars
   //  OzOled.printBigNumber(tmp);
 }
+
+
 
 
 // ------------------------------------------------------------------
@@ -81,4 +74,16 @@ void displayPreCharge(byte value1, byte value2) {      //  0 - 15 in each
   // value2 - higher value = brighter
   OzOled.sendCommand(SET_DisplayPreCharge);
   OzOled.sendCommand((value2 << 4) | value1);
+}
+
+void makeRfDataPacket(void) {
+  RfData[0] = tempPos;
+  RfData[1] = wholeDegreeC;
+  RfData[2] = decimalDegreeC;
+
+  //// Set hardcode values to test Tx
+  //  RfData[0] = 0;   // should make negative value for testing
+  //  RfData[1] = 13;   // Whole Degrees
+  //  RfData[2] = 63;   // Decimal Segment - should be 0-9 only
+
 }
