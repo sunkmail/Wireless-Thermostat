4b022202f879c9cb772e508733ea3f7e0743176b
Scott <sunkmail@gmail.com>
2018-11-14 16:20:29 -0800

    Update E_TempFunctions.ino

--- Remote Node/E_TempFunctions.ino
+++ Remote Node/E_TempFunctions.ino
@@ -1,10 +1,10 @@
 void getRawTemp(void) { // Get Raw Temperature Data
   oneWire.reset();
-  oneWire.write(0xCC);  // Bypass ROM command - since only 1 sensor used
-  oneWire.write(0xBE);  // Request Read Scratchpad
+  oneWire.write(0xCC);          // Bypass ROM command - since only 1 sensor used
+  oneWire.write(0xBE);          // Request Read Scratchpad
   rawTemp[0] = oneWire.read();
   rawTemp[1] = oneWire.read();
-  oneWire.reset();        // Send Reset to stop sending or rest of scratchpad
+  oneWire.reset();              // Send Reset to stop sending or rest of scratchpad
 }
 
 void requestTemp(void) {
@@ -13,30 +13,29 @@ void requestTemp(void) {
   oneWire.write(0x44);  // Request Temp Conversion
 }
 
-void ConvertRawTomDegreeC(void) {     // ConvertRawTomDegreeC
+void ConvertRawTemp(void) {     // ConvertRawTemp
+  /*  GLOBALS
+     byte wholeDegreeC = 0;    // Operating range well below range of byte
+     byte decimalDegreeC = 0;  // Only send 1 Sig. Bit
+  */
+  int _working = 0;             // Container for maths
 
-  int tempwhole;          // temperature in whole numbers
-  int tempdecimal;        // temperature decimal portion
-  
-  mDegreeC = ((int(rawTemp[1]) << 8) | rawTemp[0]);     // combine 2 byte value to single int
-
-  if (rawTemp[1] >= 0x80)       // if sign bit set, temp is negative
+  _working = ((int(rawTemp[1]) << 8) | rawTemp[0]);     // combine 2 byte value to single int
+  if (rawTemp[1] >= 0x80)                               // if sign bit set, temp is negative
   {
-    tempPos = false;
-    mDegreeC = -mDegreeC;       // Flip to positive value - removes 2's compliment
+    tempPos = false;                                    // set sign byte to false (temp is Neg) 
+    _working = -_working;                               // Flip to positive value - removes 2's compliment
   }
   else
     tempPos = true;
 
-  tempwhole = mDegreeC >> 4;  // get temp value of whole degrees
-
-  tempdecimal = mDegreeC & 0x000F;    // Clear all bits above zero
-  tempdecimal = tempdecimal * 625;     // value in 1/10000 of degrees ... 5000 = 0.5C
-
-  mDegreeC = ((tempwhole * 1000) + (tempdecimal / 10)); // drop least sig. digit to make into 1/1000 of a degree ... 500 = 0.5C
-
-// Below - not really needed, going to only have 1 decimal place in the end anyway.
-//  if (tempdecimal % 10 >= 5)                            // if the dropped digit 5 or more
-//    mDegreeC++;                                         // Round up
+    wholeDegreeC = _working >> 4;           // get temperature value of whole degrees
+  
+    _working = _working & 0x000F;           // Clear all bits above zero
+    _working = _working * 625;              // convert sensor reading into 1/10000 of degrees ... 5000 = 0.5C
+  
+    decimalDegreeC = (_working / 1000);     // drop least sig. digits to make into 1/10 of a degree ... 5 = 0.5C
+    if (_working % 1000 >= 500)             // if the dropped digit 5 or more
+      decimalDegreeC++;                     // Round up
 
 }
