c44862215de135c428eecb757b4553af38c3a134
Scott <sunkmail@gmail.com>
2018-11-14 16:17:50 -0800

    Update ATtiny_Tx_Thermostat_TinyOzOLED.ino

--- Remote Node/ATtiny_Tx_Thermostat_TinyOzOLED.ino
+++ Remote Node/ATtiny_Tx_Thermostat_TinyOzOLED.ino
@@ -1,27 +1,30 @@
-
-
+//  Must be set to 8MHz for RF to work?? - NEED to Confirm 
+//  
 /*
    To Do:
 
-      -) Remove unneeded font characters to save space?
-      -) Make Temperature display routine - mDegreeC to (-)xx.x
-      0) Need pull-up on Reset? - seems to work without
-      2) Add RF Tx'r - RFTransmitter.h - Some added to check compile size.  TinyRH too big
-      3) Add power saving
-      4) Optimise Code
-      5) Make larger font??      
+      - Remove unneeded font characters to save space?   . Make Local copy of Library?
+      - Use Timer ISR for sending data?
+      - Add power saving
+           - Sleep Modes?
+           - Disable ACC?
+           - Input with internal pull-ups for unused pins
+           - check nick gammon webpage for ideas
+      - Optimise Code
+      - Make larger font??      
       
       Todo's completed:
       
       - Confirm / Calibrate Temp sensor - Tested waterproof sensor - within 1/8 degree in ice water.
+      - Need pull-up on Reset? - No - via forums ... seems to work with internal Pull up 
+      - Add RF Tx'r - RFTransmitter.h
+
 */
 
 #include <TinyWireM.h>
 #include <TinyOzOLED.h>
 
 #include <OneWire.h>
-//#include <DallasTemperature.h>
-//#include <DS18B20.h>   Possible smaller Altrnative to above - need to confirm
 
 #include <RFTransmitter.h>    // https://andreasrohner.at/posts/Electronics/New-Arduino-library-for-433-Mhz-AM-Radio-Modules/
 
@@ -69,28 +72,34 @@ const byte vCom = 0;
 // Setup a oneWire instance to communicate with any OneWire devices (not just Maxim/Dallas temperature ICs)
 OneWire oneWire(ONE_WIRE_BUS);
 
-byte rawTemp[2];              // Raw Sensor data - bypassing Dallas Library
+byte rawTemp[2];              // Raw Sensor data - bypassing Dallas Library for effeciency
 
 unsigned long lastTempRequest = 0;
 const int delayInMillis = 750 / (1 << (12 - Resolution));
 
-bool tempPos = true;    // Is the temp 0 or above
-int mDegreeC = 0;       // Temperature in mili-Degrees C ... 12345 = 12.345C
+byte tempPos = true;    // Is the temp 0 or above
 
+byte wholeDegreeC = 0;    // Operating range well below range of byte
+byte decimalDegreeC = 0;  // Only send 1 Sig. Bit
 
 
 // **************************************************************
 // ****************** RF Module Set-up **************************
 /*
   RFNode List so far:
-    0 = - Not used in RF for this application
+    0 = - Not used in RF for this application - (Library default)
     1 = Bedroom Temperature
     2 = Den Temperature Sensor
     3 = Outside Temperature Sensor
 */
-const byte RFNodeID = 1;                    // Set this unit to Node # expected by Rx unit  - Rx unit uses this to confirm valid message
+const byte RfNodeID = 1;                    // Set this unit to Node # to identify signal source
 
-RFTransmitter RFTx(RadioOut_PIN,RFNodeID);               // Set up RF Tx library on RadioOut Pin, set NodeID
+unsigned int RfPulseLength = 250;           // Time of each pulse section in uSec  - Bigger = more stable, but slower 
+unsigned int RfBackoffDelay = 100;          // time in milliseconds between the repeated transmissions
+byte RfResendCount = 2;                     // number of times the transmission of a packet is repeated.  More = Better data integrity, but slower
+
+//RFTransmitter(byte outputPin, byte nodeId = 0, unsigned int pulseLength = 100, unsigned int backoffDelay = 100, byte resendCount = 1) 
+RFTransmitter RfTx(RadioOut_PIN,RfNodeID,RfPulseLength,RfBackoffDelay,RfResendCount);  // Set up RF Tx library on RadioOut Pin, set NodeID
 
 
 
@@ -98,3 +107,5 @@ RFTransmitter RFTx(RadioOut_PIN,RFNodeID);               // Set up RF Tx library
 // **************** Global Variables ************
 unsigned long currentMillis;      // Working/scratchpad variable for checking times
 
+const byte RfBytesToSend = 3;
+byte RfData[RfBytesToSend];   // Data Packet of 3 bytes:  [tempPos, WholeDegreeC, DecimalDegreeC]
