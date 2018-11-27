a3e59417607dbed300367dcf788a86ae0b655aa5
Scott <sunkmail@gmail.com>
2018-11-27 03:36:14 -0800

    Update Z_Interrupts.ino

--- Main Unit/Z_Interrupts.ino
+++ Main Unit/Z_Interrupts.ino
@@ -2,6 +2,7 @@
 // INTERRUPT     INTERRUPT     INTERRUPT     INTERRUPT     INTERRUPT
 // ------------------------------------------------------------------
 void isr_EncoderKnob()  {
+  
   static unsigned long lastInterruptTime = 0;       // Static =  Variable to retain value (like global) but only used in this routine.  Doesn't need to be volitle as only called from within the isr.
   unsigned long interruptTime = millis();
   
@@ -10,13 +11,16 @@ void isr_EncoderKnob()  {
     if (digitalRead(EncodeB_PIN) == LOW)    // If encoder moving Counter-Clockwise
     {
       if(virtualPosition > 0)
-        virtualPosition-- ;             
+        virtualPosition-- ;
+      else
+        virtualPosition = EncoderMaxPossible;     // If already at lowest value, roll over to Max Value              
       
-       
     }
     else {
-      if(virtualPosition < 100)
-        virtualPosition++ ;             // Don't increment above 100%
+      if(virtualPosition < EncoderMaxPossible)
+        virtualPosition++ ;             // Don't increment above 'EncoderMaxPossible' Const
+      else
+        virtualPosition = 0;            // If already at Max Value, roll over to 0
       
       
     }
