940fd699d7d2fcb453de815ecd168f0a7297e10e
Scott <sunkmail@gmail.com>
2019-05-10 10:13:16 -0700

    Update G_RFRx.ino

--- Main Unit/G_RFRx.ino
+++ Main Unit/G_RFRx.ino
@@ -1,6 +1,11 @@
 
+//Sketch uses 16110 bytes (52%) of program storage space. Maximum is 30720 bytes.
+//Global variables use 1129 bytes (55%) of dynamic memory, leaving 919 bytes for local variables. Maximum is 2048 bytes.
+
 void getRfData(void) {
-  byte msg[MAX_PACKAGE_SIZE];   // Make smaller to save Memory, if needed.  Default 84.  I use 3?
+//  byte msg[MAX_PACKAGE_SIZE];   // Make smaller to save Memory, if needed.  Default 84.  I use 3?
+  byte msg[10];                 // Make smaller to save Memory, if needed.  Default 84.  I use 3?
+                                    // Try setting to 10?
   byte packageId = 0;           // Package identifier ... 0 - 255
   byte len = 0;                 // Part of example - Not needed for my usage
   byte nodeId = 0;              // Default to unused value.
@@ -11,7 +16,6 @@ void getRfData(void) {
     2 = Den Temperature Sensor
     3 = Outside Temperature Sensor
   */
-
 
 
   if (RfRx.ready()) {                 // If data recieved, and not a duplicate
@@ -31,42 +35,42 @@ void getRfData(void) {
     for (byte i = 0; i <= 2; i++) {       // For 0 -> (RfBytesToSend-1), transfer data to appropriate location
       tempData[nodeId][i] = msg[i];
     }
-    tempData[nodeId][3] = packageId;      // Store latest PackageId
+    tempData[nodeId][arrayPackageID] = packageId;      // Store latest PackageId
     tempDataRxTime[nodeId] =  currentMillis;   // Sore time of Rx
 
 
     debugPrint("", 1);
-    debugPrint("Package: ");
+    debugPrint(F("Package: "));
     debugPrint(packageId, 1);
-    debugPrint("RF Node: ");
+    debugPrint(F("RF Node: "));
     debugPrint(nodeId, 1);
-    debugPrint("Message: ");
+    debugPrint(F("Message: "));
     if (msg[0] == 0)
-      debugPrint("-");
+      debugPrint(F("-"));
     else
-      debugPrint("+");
+      debugPrint(F("+"));
 
     debugPrint(msg[1]);
-    debugPrint(".");
+    debugPrint(F("."));
     debugPrint(msg[2]);
-    debugPrint(" C", 1);
+    debugPrint(F(" C"), 1);
 
     debugPrint("", 1);
-    debugPrint("Current Status:", 1);
-    debugPrint("   Setpoint: ");
+    debugPrint(F("Current Status:"), 1);
+    debugPrint(F("   Setpoint: "));
     debugPrint(tempSetpoint, 1);
-    debugPrint("   Controlled by :");
+    debugPrint(F("   Controlled by :"));
     debugPrint((char*)rmName[controlNode]);           // Print room name for Node, char array,1);
-    debugPrint(" currently at: ");
+    debugPrint(F(" currently at: "));
     debugPrint(tempData[controlNode][arrayWholeDegrees], 1);
-    debugPrint("   Heater is: ");
+    debugPrint(F("   Heater is: "));
     if (digitalRead(HeatOn_PIN) == 0)
-      debugPrint("OFF", 1);
+      debugPrint(F("OFF"), 1);
     else
-      debugPrint("ON", 1);
+      debugPrint(F("ON"), 1);
 
     //    debugPrint("",1);
-    //    debugPrint("tempData[nodeId] ");
+    //    debugPrint(F("tempData[nodeId] "));
     //    debugPrint(tempData[nodeId][0]);
     //    debugPrint(tempData[nodeId][1]);
     //    debugPrint(tempData[nodeId][2],1);
